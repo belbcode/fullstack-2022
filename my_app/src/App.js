291b5848601a69ada6e999f4587b2692ec8c7ad8
belbWindows <belbcode@gmail.com>
2022-07-20 16:09:01 -0700

    1.11

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -60,77 +60,140 @@ import { useState } from 'react'
 // }
 
 
-const ButtonSec = (props) => {
-  let elements = []
-  props.list.forEach(element => {
-    elements.push(<div key={element.text}><button onClick ={element.set}>{element.text}</button></div>)
-  });
-  return elements
-}
+// const ButtonSec = (props) => {
+//   let elements = []
+//   props.list.forEach(element => {
+//     elements.push(<div key={element.text}><button onClick ={element.set}>{element.text}</button></div>)
+//   });
+//   return elements
+// }
 
-const TotalSec = (props) => {
-  let elements = [];
-  let count = 0
-  for(let element of props.list) {
-    count += element.total
-    elements.push(<td key={element.name}> {element.name} {element.total}</td>);
-  }
-  if(count === 0) {
-    return (
-      <div>No feedback yet</div>
+// const TotalSec = (props) => {
+//   let elements = [];
+//   let count = 0
+//   for(let element of props.list) {
+//     count += element.total
+//     elements.push(<td key={element.name}> {element.name} {element.total}</td>);
+//   }
+//   if(count === 0) {
+//     return (
+//       <div>No feedback yet</div>
+//     )
+//   }
+//   return elements
+// }
+
+// const Stats = (props) => {
+//   const total = (props) => {
+//     let count = 0
+//     Object.values(props).forEach(value => {
+//       count += value
+//     })
+//     return count
+//   };
+//   const average = (props.good - props.bad) / total(props)
+//   const percent = (prop.good/total)*100
+//   return (
+//     <div>
+//       <div>Average: {average}</div>
+//       <div>Percent: {percent}</div>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const [good, setGood] = useState(0)
+//   const [neutral, setNeutral] = useState(0)
+//   const [bad, setBad] = useState(0)
+
+//   const theList = [
+//     {set : () => {setGood(good+1)}, text : "Good" },
+//     {set : () => {setNeutral(neutral+1)}, text : "Neutral"},
+//     {set : () => {setBad(bad+1)}, text: "Bad"}
+//   ]
+//   const theTotals = [
+//     {name: "Good", total: good},
+//     {name: "Neutral", total: neutral},
+//     {name: "Bad", total: bad}
+//   ]
+
+//   return (
+//     <div>
+//       <h1>Give Feedback</h1>
+//       <ButtonSec list = {theList} />
+//       <h1>Statistics</h1>
+//       <table>
+//         <col>
+//           <TotalSec list = {theTotals}/>
+//         </col>
+//         <col>
+
+//         </col>
+
+//       </table>
+//     </div>
+//   )
+// }
+
+const Button = (props) => {
+  let rtnElements = []
+  props.array.forEach((element)=> {
+    rtnElements = rtnElements.concat(
+      <div>
+        <button onClick={element.fn}>{element.text}</button>
+      </div>
+    )
+  })
+  return rtnElements
+}
+const Statistics = (props) => {
+  let data = []
+  for(let i = 0; i < props.array.length; i++) {
+    data = data.concat(
+      <tr>
+        <td>{props.array[i].label}</td>
+        <td>{props.array[i].stat}</td>
+      </tr>
     )
   }
-  return elements
-}
-
-const Stats = (props) => {
-  const total = (props) => {
-    let count = 0
-    Object.values(props).forEach(value => {
-      count += value
-    })
-    return count
-  };
-  const average = (props.good - props.bad) / total(props)
-  const percent = (prop.good/total)*100
   return (
     <div>
-      <div>Average: {average}</div>
-      <div>Percent: {percent}</div>
+      <table>
+        <tbody>
+        {data}
+        </tbody>
+      </table>
     </div>
   )
 }
 
+function ArrOfObj(array1, array2) {
+  let arr = []
+  for(let i = 0; i < array1.length; i++) {
+    arr.push({label: array1[i], stat: array2[i]})
+  }
+  return arr
+}
+
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
+
+  const [good, setgood] = useState(0)
+  const [neut, setNeut] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const theList = [
-    {set : () => {setGood(good+1)}, text : "Good" },
-    {set : () => {setNeutral(neutral+1)}, text : "Neutral"},
-    {set : () => {setBad(bad+1)}, text: "Bad"}
-  ]
-  const theTotals = [
-    {name: "Good", total: good},
-    {name: "Neutral", total: neutral},
-    {name: "Bad", total: bad}
-  ]
+  const buttons = [{fn:() => setgood(good+1), text: "good"},{fn:()=> setNeut(neut+1), text: "neutral"},{fn:()=> setBad(bad+1), text: "bad"}]
+
+  const labels = ["good", "nuetral", "bad", "all", "average", "positive"]
+  const stats = [good, neut, bad, good+neut+bad, (good-bad)/(good+neut+bad), (good /(good+neut+bad))*100 + "%"]
+
+  const pack = ArrOfObj(labels, stats)
 
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <ButtonSec list = {theList} />
-      <h1>Statistics</h1>
-      <table>
-        <col>
-          <TotalSec list = {theTotals}/>
-        </col>
-        <col>
-
-        </col>
-
-      </table>
+      <h1>give feedback</h1>
+      <Button array = {buttons} />
+      <h1>statistics</h1>
+      <Statistics array = {pack}/>
     </div>
   )
 }
