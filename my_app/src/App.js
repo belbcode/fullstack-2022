bb7db0265849b2340769f67d703dc801e2830e41
belbWindows <belbcode@gmail.com>
2022-08-04 19:27:28 -0700

    practice 2.6-2.10

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,53 +1,56 @@
-import defaultExport from './components/Course'
-const Course = defaultExport;
+import { useState } from 'react'
+import Note from './components/Note'
 
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const noteObj = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1
     }
-  ]
+    setNotes(notes.concat(noteObj))
+    setNewNote('')
+  }
 
-  return <Course course = {courses} />
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+
+  }
+  const notesToShow = showAll
+    ? notes
+    :notes.filter(note => note.important)
+    //this is an if statement in disguise
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote} 
+          onChange={handleNoteChange}
+        />
+        <button type='submit'>save</button>
+      </form>
+    </div>
+  )
 }
 
-export default App
+export default App 
